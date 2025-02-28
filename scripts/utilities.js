0c1b7639705b0ccee8386f7ae30084379bcc430d
code-shams <code.meshams@gmail.com>
2025-02-28 22:55:49 +0600

    Added Tasks Section and Activity Log Section

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,3 +1,12 @@
 // * Navbar Rightside buttons
 const completedTasksCounter = document.getElementById("completed-tasks-counter");
 const themeBtn = document.getElementById("theme-btn");
+// * Task Information
+const taskDecrement = document.getElementById("task-decrement");
+const discoverBlog = document.getElementById("discover-blog");
+const taskInformationDay = document.getElementById("task-information-day");
+const taskInformationDate = document.getElementById("task-information-date");
+// * Activity Log
+const clearHistoryBtn = document.getElementById('clear-history-btn');
+const activityLogBody = document.getElementById("activity-log-body")
+console.log(clearHistoryBtn);
